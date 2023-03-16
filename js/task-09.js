6bdc99d5503cfd5bc3fc03e37bc6ff7d9f67d219
lysbrodya <110303676+lysbrodya@users.noreply.github.com>
2023-03-16 23:45:29 +0200

    09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,9 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const btnChangeColor = document.querySelector(".change-color");
+const lol = btnChangeColor.addEventListener("click", (el) => {
+  body.style.backgroundColor = getRandomHexColor();
+});
